48baf038d1fcacac518a8bc46c6a977c34faae86
Boris Vinogradov <no111u3@gmail.com>
2018-04-10 22:12:00 +0300

    Update build system
    [*] Change chain depends to weak project

--- cmake/arm.cmake
+++ cmake/arm.cmake
@@ -1,7 +1,5 @@
-set(CMAKE_SYSTEM_PROCESSOR arm)
-
 if (CPU STREQUAL "cm4")
-    set(CPU_FLAGS "-mcpu=cortex-m4")
+    set(CPU_FLAGS "-mcpu=cortex-m4" PARENT_SCOPE)
 else()
     message("You need to define cpu for current vendor")
 endif()
